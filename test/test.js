b7997b60366b9a25b772ed38cb5b0d7f5b83a901
Muhammed-Emam <eng.muhammed.imam@gmail.com>
2023-08-29 22:37:03 +0300

    adding fixes to the test

--- test/test.js
+++ test/test.js
@@ -1,20 +1,20 @@
-// const request = require('supertest');
-// const app = require('../index.js');
+const request = require('supertest');
+const app = require('../index.js');
 
-// describe('GET /will', function() {
-//     it('responds with hello world', function(done) {
-//         request(app)
-//             .get('/will')
-//             .expect(200) // Expected HTTP status code
-//             .expect({ response: 'Hello World' }) // Expected response JSON object
-//             .end(done);
-//     });
-// });
-
-var request = require('supertest');
-var app = require('../index.js');
 describe('GET /will', function() {
-    it('respond with hello world', function(done) {
-        request(app).get('/will').expect('{ "response": "Hello World" }', done);
+    it('responds with hello world', function(done) {
+        request(app)
+            .get('/will')
+            .expect(200) // Expected HTTP status code
+            .expect({ response: 'Hello World' }) // Expected response JSON object
+            .end(done);
     });
 });
+
+// var request = require('supertest');
+// var app = require('../index.js');
+// describe('GET /will', function() {
+//     it('respond with hello world', function(done) {
+//         request(app).get('/will').expect('{ "response": "Hello World" }', done);
+//     });
+// });
