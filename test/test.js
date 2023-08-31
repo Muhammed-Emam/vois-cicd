b447ead722a1ca1a1bd160d308d60cad99f8ed51
Muhammed-Emam <118661263+Muhammed-Emam@users.noreply.github.com>
2023-08-31 19:04:37 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,20 +1,7 @@
-// var request = require('supertest');
-var assert = require('assert'); // Import the assert module
-var app = require('../index.js');
 var request = require('supertest');
-
+var app = require('../index.js');
 describe('GET /will', function() {
     it('respond with hello world', function(done) {
-        request(app)
-            .get('/will')
-            .expect(200)
-            .end(function(err, res) {
-                if (err) return done(err);
-
-                // Use deepEqual to compare the expected and actual responses
-                assert.deepEqual(res.body, { response: "Hello World" });
-
-                done();
-            });
+        request(app).get('/will').expect('{ "response": "Hello World" }', done);
     });
 });
